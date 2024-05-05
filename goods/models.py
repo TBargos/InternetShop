a9511e826e90da7657c04849ad27737bed4a1a5c
TBargos <bargos.work@gmail.com>
2024-05-05 10:04:41 +0300

    After adding fixtures for goods

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    # поле id прописывать не нужно, оно создаётся автоматически
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    # переопределение метода для понятного отображения инфы по новым объектам в бд (для админки)
+    def __str__(self) -> str:  # вообще этот метод возвращает инфу по экземпляру класса
+        return self.name
+
+    class Meta:     # Ручная корректировка миграции
+        db_table = 'category'   # имя таблицы
+        # имена для админ панели
+        verbose_name = 'Категорию' # в единственном числеч
+        verbose_name_plural = 'Категории' # во множественном числе
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    
+    def __str__(self) -> str:
+        return f'{self.name} Количество - {self.quantity}'
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- main/views.py
+++ main/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render # для отрисовки html страницы для отправки пользователю
 
+from goods.models import Categories # для работы с таблицей Категории в БД через запросы в Django
+
+
 def index(request):    # Контроллер главной страницы, request содержит информацию о запросе
+    categories = Categories.objects.all()
+    
     context = {
         'title': "Home - главная",
-        'content': "Магазин мебели HOME"
+        'content': "Магазин мебели HOME",
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
-def about(request):    # Контроллер about-страницы, request содержит информацию о запросе
+def about(request):    # Контроллер about-страницы, request содержит информацию о запросе    
     context = {
         'title': "О нас",
         'content': "Home - О нас",
-        'text_on_page': "Мы такие классные, что даже страшно, и вот 100500 причин:"
+        'text_on_page': "Мы такие классные, что даже страшно, и вот 100500 причин:",
     }
     return render(request, 'main/about.html', context)
