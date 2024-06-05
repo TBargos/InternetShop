b50ebf2ca455b4592d5b45db4ae13aac146189d6
TBargos <bargos.work@gmail.com>
2024-06-05 13:53:55 +0300

    Filter began to work, added urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,6 @@
 from django import template # для регистрации шаблонного тега через применение декоратора
+from django.utils.http import urlencode # лучше не путать, есть и другие модули с urlencode
+
 
 from goods.models import Categories # для работы с таблицей Категории в БД через запросы в Django
 
@@ -8,4 +10,11 @@ register = template.Library()
 
 @register.simple_tag()
 def tag_categories():
+    """Загружает список категорий как объекты из БД. Был написан и применён в base.html"""
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs): # context попадает в html шаблон из контроллера
+    query = context['request'].GET.dict() # запрашивает из запроса параметры в формате словаря
+    query.update(kwargs) # запихивает в словарь переданные именованные аргументы
+    return urlencode(query) # возвращает ключи и значения в формате для браузера
--- goods/views.py
+++ goods/views.py
@@ -6,7 +6,11 @@ from goods.models import Products
 
 def catalog(request, category_slug: str):
     """Показывает товары выбранной категории или все"""
+    
+    # Получение параметров из GET-запроса
     page = int(request.GET.get('page', 1))
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
     
     if category_slug == 'all':  # все товары
         goods = Products.objects.all()
@@ -15,13 +19,19 @@ def catalog(request, category_slug: str):
         goods: list[Products] = get_list_or_404(\
             Products.objects.filter(category__slug=category_slug))
     
+    # Если использован фильтр
+    if on_sale:     # Со скидкой
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':    # Есть запрос на сортировку
+        goods = goods.order_by(order_by)
+    
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
     
     context = {
         "title": "Home - Каталог",
         "goods": current_page,
-        "slug_url": category_slug
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
